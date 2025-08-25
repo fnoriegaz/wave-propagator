f1ca7069ba8092c62e5c3708e9304f60def7c0d4
fnoriegaz <fabiian.noriiega@gmail.com>
2025-08-25 22:42:59 +0200

    WIP. debug issue with cpml computation

--- main.cu
+++ main.cu
@@ -78,34 +78,36 @@ void CHECK_CALL(){
 // same kernel can be used for both dimensions x and y
 __global__
 void kernel_cpml(float *ax, float *bx, float freq, float dx, float dt, float r, int cpml_width, 
-		float courant_vel, float width){
+		float courant_vel, int length){
 
 	int ix = threadIdx.x + blockDim.x * blockIdx.x;
 	float lx = cpml_width * dx;
-	float d0 = -3 * log(r) / (2 * lx);
-	float fx = 0;
-	float ddx = 0;
+	float d0 = -3.0 * log(1./r) / (2. * lx * courant_vel);
 	float alphax = 0;
+	float sigma = 0.;
+	float fx = 0.;
 
 	if(ix < cpml_width){
 		fx = (cpml_width - ix - 1) * dx;
-		ddx = d0 * courant_vel * (fx / lx) * (fx / lx);
-		alphax = PI * freq * (lx - fx) / lx;
-		ax[ix] = expf(-(ddx+alphax)*dt);
-		bx[ix] = (ddx / (dx+alphax)) * (ax[ix]-1.);
+		float s = fx / lx;
+		alphax = PI * freq * (1. - s);
+		sigma = d0 * s * s;
+		ax[ix] = expf(-(sigma + alphax) * dt);
+		bx[ix] = (sigma / (sigma + alphax)) * (ax[ix] - 1.);
 	}
 
-	if(ix >= cpml_width && ix <= (width - cpml_width - 1)){
+	if(ix >= cpml_width && ix <= (length - cpml_width - 1)){
 		ax[ix] = 0.;
 		bx[ix] = 0.;
 	}
 
-	if(ix > (width - cpml_width -1) &&  ix < width){
-		fx = (ix - width + cpml_width) * dx;
-		ddx = d0 * courant_vel * (fx / lx) * (fx / lx);
-		alphax = PI * freq * (lx - fx) / lx;
-		ax[ix] = expf(-(ddx+alphax)*dt);
-		bx[ix] = (ddx / (dx+alphax)) * (ax[ix]-1.);
+	if(ix > (length - cpml_width -1) &&  ix < length){
+		fx = (ix - length + cpml_width) * dx;
+		float s = fx / lx;
+		alphax = PI * freq * (1. - s);
+		sigma = d0 * s * s;
+		ax[ix] = expf(-(sigma + alphax) * dt);
+		bx[ix] = (sigma / (sigma + alphax)) * (ax[ix] - 1.);
 	}
 }
 
@@ -118,7 +120,7 @@ void kernel_psiv(float *psivx, float *psivy, float *Vx, float *Vy, float *ax, fl
 	int iy = threadIdx.y + blockDim.y * blockIdx.y;
 	int tid = ix + iy * width;
 
-	if((ix > 3 && ix < cpml_width) || (ix > (width - cpml_width)) && iy < depth){
+	if( ((ix > 3 && ix < cpml_width) || (ix > (width - cpml_width) && ix < (width - 3) )) && iy < depth){
 		psivx[tid] = ax[ix]*psivx[tid] + bx[ix]*(
 			fdtd_coeff[0] * (Vx[tid] - Vx[tid-1]) -
 			fdtd_coeff[1] * (Vx[tid+1] - Vx[tid-2]) +
@@ -126,9 +128,9 @@ void kernel_psiv(float *psivx, float *psivy, float *Vx, float *Vy, float *ax, fl
 			fdtd_coeff[3] * (Vx[tid+3] - Vx[tid-4])) / dx;
 	}
 	
-	if((iy > 3 && iy < cpml_width) || (iy > (depth- cpml_width)) && ix < width){
+	if( ((iy > 3 && iy < cpml_width) || (iy > (depth - cpml_width) && iy < (depth - 3) )) && ix < width){
 		psivy[tid] = ay[iy]*psivy[tid] + by[iy]*(
-			fdtd_coeff[0] * (Vy[tid] - Vy[tid-1]) -
+			fdtd_coeff[0] * (Vy[tid] - Vy[tid-1*width]) -
 			fdtd_coeff[1] * (Vy[tid+1*width] - Vy[tid-2*width]) +
 			fdtd_coeff[2] * (Vy[tid+2*width] - Vy[tid-3*width]) -
 			fdtd_coeff[3] * (Vy[tid+3*width] - Vy[tid-4*width])) / dy;
@@ -145,20 +147,20 @@ void kernel_psi(float *psix, float *psiy, float *P, float *ax, float *bx, float 
 	int iy = threadIdx.y + blockDim.y * blockIdx.y;
 	int tid = ix + iy * width;
 
-	if((ix > 2 && ix < cpml_width) || (ix > (width-cpml_width)  && ix < (width-4)) && iy < depth ){
+	if( ((ix > 2 && ix < cpml_width) || (ix > (width-cpml_width)  && ix < (width-4))) && iy < depth ){
 		psix[tid] = ax[ix]*psix[tid] + bx[ix]*(
 			fdtd_coeff[0] * (P[tid+1] - P[tid]) -
 			fdtd_coeff[1] * (P[tid+2] - P[tid-1]) +
 			fdtd_coeff[2] * (P[tid+3] - P[tid-2]) -
-			fdtd_coeff[4] * (P[tid+4] - P[tid-3])) / dx;
+			fdtd_coeff[3] * (P[tid+4] - P[tid-3])) / dx;
 	}
 
-	if((iy > 2 && iy < cpml_width) || (iy > (depth-cpml_width)  && iy < (depth-4)) && ix < width ){
+	if( ((iy > 2 && iy < cpml_width) || (iy > (depth-cpml_width)  && iy < (depth-4))) && ix < width ){
 		psiy[tid] = ay[iy]*psiy[tid] + by[iy]*(
 			fdtd_coeff[0] * (P[tid+1*width] - P[tid]) -
 			fdtd_coeff[1] * (P[tid+2*width] - P[tid-1*width]) +
 			fdtd_coeff[2] * (P[tid+3*width] - P[tid-2*width]) -
-			fdtd_coeff[4] * (P[tid+4*width] - P[tid-3*width])) / dy;
+			fdtd_coeff[3] * (P[tid+4*width] - P[tid-3*width])) / dy;
 	}
 
 }
@@ -166,19 +168,26 @@ void kernel_psi(float *psix, float *psiy, float *P, float *ax, float *bx, float 
 
 __global__
 void kernel_dPdt(float *P1, float *P_1, float *Vx, float *Vy, float *vel, float *rho,
-				  int width, int depth, float dx, float dt, float *psivx, float *psivy){
+				  int width, int depth, float dx, float dy, float dt, float *psivx, float *psivy){
 
 	int ix = threadIdx.x + blockDim.x * blockIdx.x;
 	int iy = threadIdx.y + blockDim.y * blockIdx.y;
 	int tid = ix + iy * width;
 
 	if(ix > 3 && ix < (width - 3) && iy > 3 && iy < (depth - 3)){
-		P1[tid] += -1 * dt * vel[tid] * vel[tid] * rho[tid] * (
-			fdtd_coeff[0] * (Vx[tid] - Vx[tid-1] + Vy[tid] - Vy[tid-1*width]) -
-			fdtd_coeff[1] * (Vx[tid+1] - Vx[tid-2] + Vy[tid+1*width] - Vy[tid-2*width]) +
-			fdtd_coeff[2] * (Vx[tid+2] - Vx[tid-3] + Vy[tid+2*width] - Vy[tid-3*width]) -
-			fdtd_coeff[3] * (Vx[tid+3] - Vx[tid-4] + Vy[tid+3*width] - Vy[tid-4*width]) )
-			/ dx + psivx[tid] + psivx[tid];
+
+		float dvx_dx = (
+			fdtd_coeff[0] * (Vx[tid] - Vx[tid-1]) -
+			fdtd_coeff[1] * (Vx[tid+1] - Vx[tid-2]) +
+			fdtd_coeff[2] * (Vx[tid+2] - Vx[tid-3]) -
+			fdtd_coeff[3] * (Vx[tid+3] - Vx[tid-4]) ) / dx;
+		float dvy_dy = (
+			fdtd_coeff[0] * (Vy[tid] - Vy[tid-1*width]) -
+			fdtd_coeff[1] * (Vy[tid+1*width] - Vy[tid-2*width]) +
+			fdtd_coeff[2] * (Vy[tid+2*width] - Vy[tid-3*width]) -
+			fdtd_coeff[3] * (Vy[tid+3*width] - Vy[tid-4*width]) ) / dy;
+
+		P1[tid] += -1 * dt * vel[tid] * vel[tid] * rho[tid] *(dvx_dx + dvy_dy) + psivx[tid] + psivy[tid];
 	}
 }
 
@@ -192,7 +201,7 @@ void kernel_dVdt(float *P, float *Vx, float *Vy, float *rho,
 	int tid = ix + iy * width;
 
 	if(ix > 2 && ix < (width - 4) && iy < depth){
-		Vx[tid] += -1 * (2. / (rho[tid] + rho[tid+1])) * dt * (
+		Vx[tid] = Vx[tid] - (2. / (rho[tid] + rho[tid+1])) * dt * (
 			fdtd_coeff[0] * (P[tid+1] - P[tid]) - 
 			fdtd_coeff[1] * (P[tid+2] - P[tid-1]) +
 			fdtd_coeff[2] * (P[tid+3] - P[tid-2]) -
@@ -200,7 +209,7 @@ void kernel_dVdt(float *P, float *Vx, float *Vy, float *rho,
 	}
 	
 	if(iy > 2 && iy < (depth - 4) && ix < width){
-		Vy[tid] += -1 * (2. / (rho[tid] + rho[tid+1*width])) * dt * (
+		Vy[tid] = Vy[tid] - (2. / (rho[tid] + rho[tid+1*width])) * dt * (
 			fdtd_coeff[0] * (P[tid+1*width] - P[tid]) - 
 			fdtd_coeff[1] * (P[tid+2*width] - P[tid-1*width]) +
 			fdtd_coeff[2] * (P[tid+3*width] - P[tid-2*width]) -
@@ -212,7 +221,7 @@ void kernel_dVdt(float *P, float *Vx, float *Vy, float *rho,
 __global__
 void kernel_add_source(float *P, float *source, int time_sample, int sloc_x, int sloc_y,
 					   int width){
-	P[sloc_x + width * sloc_y] += 1;
+	P[sloc_x + width * sloc_y] += 10.;
 }
 
 
@@ -223,8 +232,10 @@ void propagate(float *P1, float *P_1, float *Vx, float *Vy, float *rho, float *v
 	dim3 block_size(16,16);
 	dim3 grid_size((width+block_size.x-1)/block_size.x, (depth+block_size.y-1)/block_size.y);
 	
-	float courant_velx = dx / dt * 1 / (sqrt(3) * (1225./1024 + 245./3072 + 49./5120 + 5./7168));
-	float courant_vely = dy / dt * 1 / (sqrt(3) * (1225./1024 + 245./3072 + 49./5120 + 5./7168));
+	//float courant_velx = (dx / dt) * (1 / (sqrt(3)) * (1225./1024 + 245./3072 + 49./5120 + 5./7168));
+	//float courant_vely = (dy / dt) * (1 / (sqrt(3)) * (1225./1024 + 245./3072 + 49./5120 + 5./7168));
+	float courant_velx = 1500.;
+	float courant_vely = 1500.;
 
 	float r = 1e-4;
 	float freq = 3;
@@ -234,33 +245,47 @@ void propagate(float *P1, float *P_1, float *Vx, float *Vy, float *rho, float *v
 	cudaMalloc(&psivy, width*depth*sizeof(float));
 	cudaMalloc(&psix, width*depth*sizeof(float));
 	cudaMalloc(&psiy, width*depth*sizeof(float));
-	cudaMalloc(&ax, cpml_width*width*sizeof(float));
-	cudaMalloc(&bx, cpml_width*width*sizeof(float));
-	cudaMalloc(&ay, cpml_width*depth*sizeof(float));
-	cudaMalloc(&by, cpml_width*depth*sizeof(float));
+	cudaMalloc(&ax, width*sizeof(float));
+	cudaMalloc(&bx, width*sizeof(float));
+	cudaMalloc(&ay, depth*sizeof(float));
+	cudaMalloc(&by, depth*sizeof(float));
 
 	cudaMemset(psix, 0, width*depth*sizeof(float));
 	cudaMemset(psiy, 0, width*depth*sizeof(float));
 	cudaMemset(psivx, 0, width*depth*sizeof(float));
 	cudaMemset(psivy, 0, width*depth*sizeof(float));
+	cudaMemset(ax, 0,width*sizeof(float));
+	cudaMemset(bx, 0,width*sizeof(float));
+	cudaMemset(ay, 0,depth*sizeof(float));
+	cudaMemset(by, 0,depth*sizeof(float));
 
 	kernel_cpml<<<(width+block_size.x-1)/block_size.x, block_size.x>>>(ax,bx,freq,dx,dt,r,cpml_width,
 	courant_velx,width);
-	kernel_cpml<<<(width+block_size.y-1)/block_size.y, block_size.y>>>(ay,by,freq,dy,dt,r,cpml_width,
+	kernel_cpml<<<(depth+block_size.y-1)/block_size.y, block_size.y>>>(ay,by,freq,dy,dt,r,cpml_width,
 	courant_vely,depth);
+
+	float *h_ax = (float *)malloc(width*sizeof(float));
+	float *h_bx = (float *)malloc(width*sizeof(float));
+	float *h_ay = (float *)malloc(depth*sizeof(float));
+	float *h_by = (float *)malloc(depth*sizeof(float));
+	cudaMemcpy(h_ax, ax, width*sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_bx, bx, width*sizeof(float), cudaMemcpyDeviceToHost);
+
+	for (int i=0;i<cpml_width;i++) printf("%f %f\n", h_ax[i], h_bx[i]);
+	for (int i=width-cpml_width;i<width;i++) printf("%f %f\n", h_ax[i], h_bx[i]);
 
 	for(int c=0;c<time_samples;c++){
 		std::cout << "iteration: " << c << std::endl;
-		//kernel_psiv<<<grid_size, block_size>>>(psivx,psivy,Vx,Vy,ax,bx,ay,by,dx,dy,width,depth,cpml_width);
+		kernel_psi<<<grid_size, block_size>>>(psix,psiy,P1,ax,bx,ay,by,dx,dy,width,depth,cpml_width);
+		CHECK_CALL();
+		CUDA_CHECK(cudaDeviceSynchronize());
+		kernel_psiv<<<grid_size, block_size>>>(psivx,psivy,Vx,Vy,ax,bx,ay,by,dx,dy,width,depth,cpml_width);
 		CHECK_CALL();
 		CUDA_CHECK(cudaDeviceSynchronize());
 		kernel_dVdt<<<grid_size, block_size>>>(P1,Vx,Vy,rho,width,depth,dx,dy,dt,psix,psiy);
 		CHECK_CALL();
 		CUDA_CHECK(cudaDeviceSynchronize());
-		kernel_psi<<<grid_size, block_size>>>(psix,psiy,P1,ax,bx,ay,by,dx,dy,width,depth,cpml_width);
-		CHECK_CALL();
-		CUDA_CHECK(cudaDeviceSynchronize());
-		kernel_dPdt<<<grid_size, block_size>>>(P1,P_1,Vx,Vy,vel,rho,width,depth,dx,dt,psivx,psivy);
+		kernel_dPdt<<<grid_size, block_size>>>(P1,P_1,Vx,Vy,vel,rho,width,depth,dx,dy,dt,psivx,psivy);
 		CHECK_CALL();
 		CUDA_CHECK(cudaDeviceSynchronize());
 
@@ -274,14 +299,14 @@ void propagate(float *P1, float *P_1, float *Vx, float *Vy, float *rho, float *v
 	}
 
 	free(P_h);
-	cudaFree(&psivx);
-	cudaFree(&psivy);
-	cudaFree(&psix);
-	cudaFree(&psiy);
-	cudaFree(&ax);
-	cudaFree(&bx);
-	cudaFree(&ay);
-	cudaFree(&by);
+	cudaFree(psivx);
+	cudaFree(psivy);
+	cudaFree(psix);
+	cudaFree(psiy);
+	cudaFree(ax);
+	cudaFree(bx);
+	cudaFree(ay);
+	cudaFree(by);
 }
 
 
